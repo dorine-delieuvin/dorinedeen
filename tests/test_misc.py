7eca991816804508f599a6007f7d602528c1614c
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-09-19 11:42:51 +0100

    Initial setup of the test files

---
+++ tests/test_misc.py
@@ -0,0 +1,16 @@
+import pytest
+import re
+
+from playwright.sync_api import Page, expect
+
+"""
+UI testing, using Playwright
+test of the sections visible on all pages
+or only on first connection
+of the dorinedeen.wordpress.com website
+"""
+
+
+@pytest.mark.ui
+def test_smth():
+    pass
