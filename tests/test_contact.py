58e56317a3aa5d3c29ca457f2565dc78f78e558e
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-10-05 12:48:16 +0100

    Test_contact test_contact_title_and_url

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -7,8 +7,27 @@ from playwright.sync_api import Page, expect
 UI testing, using Playwright
 test of the CONTACT page of the dorinedeen.wordpress.com website
 """
+pytestmark = pytest.mark.ui
+pytestmark2 = pytest.mark.contact
+
+page_name = "contact"
 
 
-@pytest.mark.ui
-def test_smth():
-    pass
+## Tests Setup/Cleanup
+# Go to the starting url before each test.
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each(page: Page, home_page_url):
+    page.goto(home_page_url + f"/{page_name}")
+    yield
+
+
+## Tests
+# Page header and url
+def test_contact_title_and_url(page: Page):
+    expect(page).to_have_url(re.compile(f".*/{page_name}*"))
+    expect(page.locator("h1.entry-title")).to_be_visible()
+
+
+# Form
+
+# Contact card
