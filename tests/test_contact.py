7ff20dbca40cb68f873709f995b7a63b86458c34
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-10-07 11:02:23 +0100

    Test_contact test_form_display, test_form_required_fields

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -12,6 +12,14 @@ pytestmark2 = pytest.mark.contact
 
 page_name = "contact"
 
+## Data sets for parametrization
+# required_fields = ["Field CSS locator"]
+required_fields = [
+    "label.grunion-field-label.name",
+    "label.grunion-field-label.email",
+    "label.grunion-field-label.textarea",
+]
+
 
 ## Tests Setup/Cleanup
 # Go to the starting url before each test.
@@ -29,5 +37,17 @@ def test_contact_title_and_url(page: Page):
 
 
 # Form
+@pytest.mark.contact_form
+def test_form_display(page: Page):
+    expect(page.locator("id=contact-form-7")).to_be_visible()
+    expect(page.locator("button.wp-block-button__link")).to_be_visible()
+
+
+@pytest.mark.contact_form
+@pytest.mark.parametrize("field", required_fields)
+def test_form_required_fields(page: Page, field):
+    expect(page.locator(field)).to_be_visible()
+    expect(page.locator(field)).to_have_text(re.compile(".*required.*"))
+
 
 # Contact card
